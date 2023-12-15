792603eb767f56a24356e13e911f5b2b0a174bac
Max <max@maximilian.co.nz>
2023-12-16 02:02:59 +1300

    debug thing to convert stuff to base64

--- src/Program.cs
+++ src/Program.cs
@@ -1,7 +1,22 @@
+using System.Text;
+
 class Program
 {
 	public static void Main(string[] args)
 	{
+		// Debug convert to base64 thingy
+		// TODO: Remove for production and make a standalone program for making songs
+		if (args.Length == 1)
+		{
+			// Get the bytes of the provided thing, then convert to base64
+			byte[] bytes = File.ReadAllBytes(args[0]);
+			string base64 = Convert.ToBase64String(bytes);
+			Console.WriteLine(base64);
+
+			// Quit and don't run the game
+			return;
+		}
+
 		Game.Run();
 	}
-}
+} 
